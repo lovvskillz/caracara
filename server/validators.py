ab14b9c2e30c4e91621df94371e2b29fc3438b84
lovvskillz <14542790+lovvskillz@users.noreply.github.com>
2023-01-15 23:04:36 +0100

    update cidr validator

--- server/validators.py
+++ server/validators.py
@@ -12,17 +12,18 @@ def validate_cidr_notation(ip_net: str) -> str:
         r'^((?:25[0-5]|2[0-4]?[0-9]?|[13-9][0-9]?[0-9]?|0)\.){3}(?:25[0-5]|2[0-4]?[0-9]?|[13-9][0-9]?[0-9]?|0)/(3[0-2]?|[1-2]?\d|\d)$',
         ip_net,
     ):
-        net_id, suffix = tuple(ip_net.split('/'))
+        network_prefix, suffix = tuple(ip_net.split('/'))
         suffix = int(suffix)
         if suffix < 24:
             raise ValidationError(_("The subnet mask should not be lower than 24."))
         if suffix > 29:
             raise ValidationError(_("The subnet mask should not be greater than 29."))
         remaining_bits = 32 - suffix
-        last_byte = net_id.split('.')[3]
-        if int(last_byte) != (expected_last_byte := 256 - 2**remaining_bits):
+        last_byte = int(network_prefix.split('.')[3])
+        if last_byte % (ip_net_range := 2**remaining_bits) != 0:
+            expected_last_byte = int(last_byte / ip_net_range) * ip_net_range
             expected_ip_net = (
-                f'{net_id.rsplit(last_byte, 1)[0]}{expected_last_byte}/{suffix}'
+                f'{network_prefix.rsplit(str(last_byte), 1)[0]}{expected_last_byte}/{suffix}'
             )
             raise ValidationError(
                 _("The CIDR notation should be %(ip_net)s.")

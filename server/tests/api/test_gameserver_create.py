b7e7bf98a04ffc22c5a62a6c7c9fe0987a79ceec
lovvskillz <14542790+lovvskillz@users.noreply.github.com>
2023-02-04 11:30:40 +0100

    assert gameserver properties

--- server/tests/api/test_gameserver_create.py
+++ server/tests/api/test_gameserver_create.py
@@ -1,13 +1,18 @@
+from datetime import timedelta
+
+import freezegun
+from django.utils import timezone
 from pytest import mark
 from rest_framework import status
 from rest_framework.reverse import reverse
 
-from server.models import GameSoftwareVersion, UserGameServer
+from server.models import GameSoftwareVersion, Node, UserGameServer
 
 URL = reverse('api:server:gameserver-list')
 
 
 @mark.django_db
+@freezegun.freeze_time('01-01-2023')
 def test__create_gameserver__valid(apitest, user, basic_server_setup):
     """
     Ensure that a gameserver can be created.
@@ -32,6 +37,14 @@ def test__create_gameserver__valid(apitest, user, basic_server_setup):
             'software_version': software_version.id,
         },
     )
+    gameserver = UserGameServer.objects.get()
 
     assert response.status_code == status.HTTP_201_CREATED
-    assert UserGameServer.objects.count() == 1
+    assert gameserver.server_name == server_name
+    assert gameserver.ram == ram
+    assert gameserver.disk_space == disk_space
+    assert gameserver.cores == cores
+    assert gameserver.available_until == timezone.now() + timedelta(days=period)
+    assert gameserver.own_ip
+    assert gameserver.software_version == software_version
+    assert gameserver.node == Node.objects.get()
